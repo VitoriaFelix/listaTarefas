9f3f6e02a680c01d11ccadd75b8359cedae90a2c
VitoriaFelix <vitoriafelix22@hotmail.com>
2021-10-16 13:13:48 -0300

    first commit

---
+++ src/App.styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    background-color: #17181f;
+    color: #707a81;
+    min-height: 100vh;
+`;
+
+export const Area = styled.div`
+    margin:auto;
+    max-width: 980px;
+    padding: 10px;
+`;
+
+export const Header = styled.h1`
+    margin:0;
+    border-bottom: 1px solid #444;
+    padding: 0px;
+    padding-bottom: 20p;
+    color: #fff;
+    text-align: center;
+`;
